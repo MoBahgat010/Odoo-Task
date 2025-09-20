2a164fb8ed709d5c701806e38ec5e3b0d46740a2
Mohamed Bahgat <mbahgat503@gmail.com>
2025-09-20 11:09:07 +0300

    whatsapp redirect chat

--- whatsapp_partner/__manifest__.py
+++ whatsapp_partner/__manifest__.py
@@ -1,8 +1,7 @@
 {
-    'name': 'WhatsApp Integration for Contacts',
+    'name': 'WhatsApp Redirection',
     'version': '18.0.1.0.0',
-    'summary': 'Add WhatsApp chat button',
-    'website': 'https://yourwebsite.com',
+    'summary': 'whatsapp redirect chat',
     'depends': ['contacts'],
     'data': [
         'security/ir.model.access.csv',
